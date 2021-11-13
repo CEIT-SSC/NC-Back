2b3a6c9156c5f9fbc3a17fb2d22a4f4ea3a3c285
s.amini <sina3050@gmail.com>
2021-11-14 00:28:00 +0330

    added todo

--- internal/modules/user_module.go
+++ internal/modules/user_module.go
@@ -6,7 +6,7 @@ import "github.com/ceit-ssc/nc_backend/pkg/repository"
 type UserModule struct{
 	UserRepo repository.UserRepository
 }
-//return user ID
+//TODO: DO me
 func (u *UserModule) RegisterNewUser()(int,error){
 
 	return -1, nil
