cf8b0312451fc0605221126e4f6ef816a659c640
s.amini <sina3050@gmail.com>
2021-11-15 12:36:48 +0330

    added registered user err

--- pkg/error/error.go
+++ pkg/error/error.go
@@ -6,4 +6,5 @@ var (
 	ErrTokenMissing  = errors.New("token is missing")
 	ErrInvalidToken  = errors.New("invalid token")
 	ErrNotRegistered = errors.New("invalid User id")
+	ErrUserIsRegiseterd = errors.New("User is Registered before")
 )
